c799759677c4d8adac3904f702e922eb8d9451b9
Bram Vogelaar <attachmentgenie@inuits.eu>
2015-07-02 22:58:49 +0200

    fixing test

--- spec/classes/stack_website_spec.rb
+++ spec/classes/stack_website_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 describe 'orchestrate::stack_website' do
 
+  let :facts do
+  {
+   :concat_basedir         => '/tmp',
+   :osfamily               => 'debian',
+   :operatingsystemrelease => 'wheezy'
+  }
+  end
   context 'with defaults for all parameters' do
     it { should contain_class('orchestrate::stack_website') }
   end
